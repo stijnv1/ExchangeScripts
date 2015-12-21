73c0ebc069b160d0c070ff4b218ffb7f2525ade8
Stijn Verhoeven <stijnv1@hotmail.com>
2015-12-21 13:32:51 +0100

    added export SMTP info to CSV script

---
+++ ExportDataFromExchange/ExportDataFromExchange/ExportSMTPInfoToCSV.ps1
@@ -0,0 +1,65 @@
+#
+# ExportSMTPInfoToCSV.ps1
+#
+$mailboxes = Get-Mailbox
+$exportUserArray = @()
+$Member = @{
+		MemberType = "NoteProperty"
+		Force = $true
+	}
+
+#foreach ($mailbox in $mailboxes)
+#{
+#    if ($mailbox.RecipientTypeDetails -ne "UserMailbox")
+#    {
+#        $adUser = Get-ADUser -Filter {UserPrincipalName -eq $mailbox.UserPrincipalName}
+
+#        if ($adUser.GivenName -eq $null)
+#        {
+#            Write-Host "Updating user $($adUser.UserPrincipalName)" -ForegroundColor Yellow
+#            $aduser | Set-ADUser -GivenName $mailbox.Alias -Surname $mailbox.RecipientTypeDetails
+#        }
+#    }
+#}
+
+foreach ($mailbox in $mailboxes)
+{
+    $adUser = Get-ADUser -Filter {UserPrincipalName -eq $mailbox.UserPrincipalName}
+
+    $userObject = New-Object psobject
+    
+    $userObject | Add-Member @Member -Name "GivenName" -Value $adUser.GivenName
+    $userObject | Add-Member @Member -Name "Surname" -Value $adUser.Surname
+    $userObject | Add-Member @Member -Name "MailboxType" -Value $mailbox.RecipientTypeDetails
+    $userObject | Add-Member @Member -Name "Alias" -Value $mailbox.Alias
+
+    #export the proxy addresses
+    $EmailAddressesStringArray = [string[]]$mailbox.emailaddresses
+    [int]$i = 0
+    foreach ($emailAddress in $EmailAddressesStringArray)
+    {
+        if (($emailAddress | Select-String -Pattern "SMTP:") -or ($emailAddress | Select-String -Pattern "smtp:"))
+        {
+            if (!($emailAddress | Select-String -Pattern "adam.local"))
+            {
+                $emailAddress = $emailAddress.Replace("smtp:","")
+                $emailAddress = $emailAddress.Replace("SMTP:","")
+                $userObject | Add-Member @Member -Name "EmailAddress_$i" -Value $emailAddress
+                $i++
+            }
+        }
+    }
+
+    $exportUserArray += $userObject
+
+
+    #Write-Host "AD User= $($adUser.SurName) $($adUser.GivenName)"
+    #Write-Host "Mailbox = $($mailbox.RecipientTypeDetails)" -ForegroundColor Green
+    #if ($mailbox.RecipientTypeDetails -ne "UserMailbox")
+    #{
+    #    Write-Host "Alias = $($mailbox.Alias) - number of characters =" $($mailbox.Alias).length -ForegroundColor Yellow
+    #}
+    #Write-Host "-------------------------------------"
+}
+
+$exportUserArray | Export-Csv -Path C:\Sources\userExport.csv -NoTypeInformation
